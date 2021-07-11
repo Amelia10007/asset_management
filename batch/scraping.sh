62ed408a7e0b8a2a54905b8ad78b1fcf385fc284
Amelia10007 <nat.horn.mk0426@gmail.com>
2021-07-11 22:13:28 +0900

    Fix batch message

--- batch/scraping.sh
+++ batch/scraping.sh
@@ -5,13 +5,13 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 pushd $SCRIPT_DIR
 
 started=$(date)
-echo "scraping batch started now: ${started}" >> ../log/log.log
+echo "scraping batch started at ${started}" >> ../log/log.log
 
 pushd ../rust/nicehash_scraper
 ../target/debug/nicehash_scraper >> ../../log/scraper.log
 popd
 
 finished=$(date)
-echo "scraping batch finished now: ${finished}" >> ../log/log.log
+echo "scraping batch finished at ${finished}" >> ../log/log.log
 
 popd
