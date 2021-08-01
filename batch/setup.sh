bce821a6f9e08a770af332be6d31de51d278a4ac
Amelia10007 <nat.horn.mk0426@gmail.com>
2021-08-01 12:54:08 +0900

    Improve setup script

--- batch/setup.sh
+++ batch/setup.sh
@@ -1,11 +1,27 @@
+# NOTE: Some commands may need super user previledge
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 pushd $SCRIPT_DIR
 
+# Build
 pushd ../rust
-
 cargo build
-
 popd
+
+# Start DB
+pushd ../docker-autotrader-db
+docker-compose up
+popd
+
+# Setup cron
+crontab -l > tmpcron
+echo >> tmpcron
+echo '# Automatically appended schedule by asset_management' >> tmpcron
+echo "*/5 * * * * ${SCRIPT_DIR}/scraping.sh" >> tmpcron
+crontab tmpcron
+
+service cron enable
+service cron restart
 
 popd
