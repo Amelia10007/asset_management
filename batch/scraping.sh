9a8f0477fb53300dab35751ccc2211a2ad793915
Amelia10007 <nat.horn.mk0426@gmail.com>
2021-08-10 14:55:22 +0900

    Refactor batch shell

--- batch/scraping.sh
+++ batch/scraping.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-CHECK_FILE="../log/scraping_now.tmp.log"
+LOG_DIR=$SCRIPT_DIR/../log
+CHECK_FILE=$LOG_DIR/scraping_now.tmp.log
 
 pushd $SCRIPT_DIR
 
 if [-e $CHECK_FILE]; then
-    echo 'Previous batch not finished yet' >> ../log/log.log
+    echo 'Previous batch not finished yet' >> $LOG_DIR/log.log
 fi
 
 echo 'Batch is running. Do not delete this file manually.' > $CHECK_FILE
 
 started=$(date)
-echo "scraping batch started at ${started}" >> ../log/log.log
+echo "scraping batch started at ${started}" >> $LOG_DIR/log.log
 
 pushd ../rust/nicehash_scraper
-../target/debug/nicehash_scraper >> ../../log/scraper.log
+../target/debug/nicehash_scraper >> $LOG_DIR/scraper.log 2>&1
 popd
 
 pushd ../rust/nicehash_speculator
-../target/debug/nicehash_speculator >> ../../log/speculator.log
+../target/debug/nicehash_speculator >> $LOG_DIR/speculator.log 2>&1
 popd
 
 finished=$(date)
-echo "scraping batch finished at ${finished}" >> ../log/log.log
+echo "scraping batch finished at ${finished}" >> $LOG_DIR/log.log
 
 rm $CHECK_FILE
 
